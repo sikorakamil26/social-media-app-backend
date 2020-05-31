6fe08d7701c59f8c9e0534beed20fd76142af480
Kamil <sikorakamil26>
2020-05-31 14:57:20 +0200

    fixed validators

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,17 +1,17 @@
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
 const isEmpty = (string) => {
   if (string.trim() === "") return true;
   else return false;
 };
 
-const isEmail = (email) => {
-  const reqEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (email.match(reqEx)) return true;
-  else return false;
-};
-
 exports.validateSignupData = (data) => {
   let errors = {};
-  // Validation
+
   if (isEmpty(data.email)) {
     errors.email = "Must not be empty";
   } else if (!isEmail(data.email)) {
@@ -19,11 +19,9 @@ exports.validateSignupData = (data) => {
   }
 
   if (isEmpty(data.password)) errors.password = "Must not be empty";
-
   if (data.password !== data.confirmPassword)
     errors.confirmPassword = "Passwords must match";
-
-  if (isEmpty(data.handle)) errors.userHandle = "Must not be empty";
+  if (isEmpty(data.handle)) errors.handle = "Must not be empty";
 
   return {
     errors,
@@ -37,7 +35,6 @@ exports.validateLoginData = (data) => {
   if (isEmpty(data.email)) errors.email = "Must not be empty";
   if (isEmpty(data.password)) errors.password = "Must not be empty";
 
-  if (Object.keys(errors).length > 0) return response.status(400).json(errors);
   return {
     errors,
     valid: Object.keys(errors).length === 0 ? true : false,
@@ -48,14 +45,12 @@ exports.reduceUserDetails = (data) => {
   let userDetails = {};
 
   if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
-
   if (!isEmpty(data.website.trim())) {
-    // https or http -> save as it is, but if it doesnt contain -> add http
+    // https://website.com
     if (data.website.trim().substring(0, 4) !== "http") {
       userDetails.website = `http://${data.website.trim()}`;
     } else userDetails.website = data.website;
   }
-
   if (!isEmpty(data.location.trim())) userDetails.location = data.location;
 
   return userDetails;
