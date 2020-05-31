34d645935d59f1fde1ee4a17de60b25d71dfc8fa
Kamil <sikorakamil26>
2020-05-31 14:40:52 +0200

    fixed errors

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -89,13 +89,11 @@ exports.login = (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      if (err.code === "auth/wrong-password") {
-        return res
-          .status(403)
-          .json({ general: "Wrong credentials, please try again" });
-      } else {
-        return res.status(500).json({ error: err.code });
-      }
+      // auth/wrong-password
+      // auth/user-not-user
+      return res
+        .status(403)
+        .json({ general: "Wrong credentials, please try again" });
     });
 };
 
